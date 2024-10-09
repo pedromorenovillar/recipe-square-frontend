be66c5c66d142f82f1e03c49d7628d3cc14535d6
Pedro José Moreno Villar <151030573+pedromorenovillar@users.noreply.github.com>
2024-10-09 13:16:18 +0200

    added icons and metadata in package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/logo-transparent.png'
 import './App.css'
 
 function App() {
@@ -12,9 +11,7 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
       </div>
       <h1>Vite + React + Pedro</h1>
       <div className="card">
